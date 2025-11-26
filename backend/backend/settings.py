ed680f2b1d773b0c0c1631ce7cf4c6130b2bf0ff
Muktadirul <muktadirul.05@gmail.com>
2025-11-26 14:55:40 +0000

    gtm

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -93,7 +93,7 @@ MIDDLEWARE = [
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',  # make sure Redis is installed
+        'LOCATION': 'redis://127.0.0.1:6379/2',  # make sure Redis is installed
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
